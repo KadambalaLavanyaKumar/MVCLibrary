3273d13de3017e23a67f7f8ad4450ddafc6f07e4
KadambalaLavanyaKumar <lavanyakumar.cse@gmail.com>
2023-04-20 17:07:40 +0530

    Add project files.

---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context=new LibraryContext(serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if (context.Book.Any())
+                {
+                    return;
+                }
+                context.Book.AddRange(new Book { Title = "C# project", CallNumber = "RDX 2023",Author="ram" },
+                    new Book { Title = "Android project", CallNumber = "TDX 2024" ,Author="sam"});
+                context.SaveChanges();
+            }
+        }
+    }
+}
